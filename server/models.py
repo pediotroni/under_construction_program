59fe343e96887b48588c7b8922789b21ccf47fd1
Pedio Troni <36533556+pediotroni@users.noreply.github.com>
2025-08-01 19:00:03 +0330

    Add files via upload

--- server/models.py
+++ server/models.py
@@ -1,5 +1,8 @@
 from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
 from datetime import datetime
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey, Enum, DateTime
+from enum import Enum as PyEnum
+
 
 class Message(Base):
     __tablename__ = "messages"
@@ -10,3 +13,20 @@ class Message(Base):
     content = Column(String(500))  # محدودیت 500 کاراکتر
     timestamp = Column(DateTime, default=datetime.utcnow)
     is_read = Column(Boolean, default=False)
+    
+
+class FriendshipStatus(PyEnum):
+    PENDING = "pending"
+    ACCEPTED = "accepted"
+    REJECTED = "rejected"
+    BLOCKED = "blocked"
+
+class Friendship(Base):
+    __tablename__ = "friendships"
+    
+    id = Column(Integer, primary_key=True, index=True)
+    requester_mobile = Column(String(11), ForeignKey("users.mobile"))
+    receiver_mobile = Column(String(11), ForeignKey("users.mobile"))
+    status = Column(Enum(FriendshipStatus), default=FriendshipStatus.PENDING)
+    created_at = Column(DateTime, default=datetime.utcnow)
+    updated_at = Column(DateTime, onupdate=datetime.utcnow)
